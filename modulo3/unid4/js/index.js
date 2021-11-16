32ad73c6ebffa525427f13d2cce7ac6e7f979c81
battagliajose <battagliajose@gmail.com>
2021-11-16 15:41:57 -0300

    Mod4 - Unid 4 - Ejercicios JS

---
+++ modulo3/unid4/js/index.js
@@ -0,0 +1,11 @@
+var boxOne = document.querySelector(".one");
+
+console.log(boxOne);
+
+function changeColor()
+{
+    alert("Click!");
+}
+
+boxOne.addEventListener("click", changeColor, false);
+
