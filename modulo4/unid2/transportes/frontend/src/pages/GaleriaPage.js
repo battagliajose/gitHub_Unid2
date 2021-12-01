2e402974f407e224644b944221b1b201017906cb
battagliajose <battagliajose@gmail.com>
2021-12-01 13:28:54 -0300

    Migracion Galeria a Bootstrap React

--- modulo4/unid2/transportes/frontend/src/pages/GaleriaPage.js
+++ modulo4/unid2/transportes/frontend/src/pages/GaleriaPage.js
@@ -1,8 +1,83 @@
 import '../styles/components/pages/GaleriaPage.css'
+import Carousel from 'react-bootstrap/Carousel';
+
 const Galeria = (props) => {
     return (
-        <div className="mainDiv">
-            <h1 Style="text-align: center;">EN CONSTRUCCION (Migrando Bootstrap)</h1>
+        <div className="mainDiv centerText">
+            <h1>Galeria</h1>
+            <Carousel className="carouselMain">
+                <Carousel.Item className="carouselItem">
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG1.jpg"
+                    alt="First slide"
+                    />
+                    {/* <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption> */}
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG2.jpg"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG3.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG4.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG5.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="img/ImagenG6.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 }
