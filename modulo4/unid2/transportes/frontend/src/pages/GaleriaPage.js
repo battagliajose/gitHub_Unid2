1444e57545b0e23f010d2a53258f6de2e8f27c41
battagliajose <battagliajose@gmail.com>
2021-12-01 13:46:11 -0300

    Cambios estilos galeria

--- modulo4/unid2/transportes/frontend/src/pages/GaleriaPage.js
+++ modulo4/unid2/transportes/frontend/src/pages/GaleriaPage.js
@@ -6,16 +6,12 @@ const Galeria = (props) => {
         <div className="mainDiv centerText">
             <h1>Galeria</h1>
             <Carousel className="carouselMain">
-                <Carousel.Item className="carouselItem">
+                <Carousel.Item>
                     <img
                     className="d-block w-100"
                     src="img/ImagenG1.jpg"
                     alt="First slide"
                     />
-                    {/* <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -23,11 +19,6 @@ const Galeria = (props) => {
                     src="img/ImagenG2.jpg"
                     alt="Second slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -35,11 +26,6 @@ const Galeria = (props) => {
                     src="img/ImagenG3.jpg"
                     alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -47,11 +33,6 @@ const Galeria = (props) => {
                     src="img/ImagenG4.jpg"
                     alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -59,11 +40,6 @@ const Galeria = (props) => {
                     src="img/ImagenG5.jpg"
                     alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -71,11 +47,6 @@ const Galeria = (props) => {
                     src="img/ImagenG6.jpg"
                     alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
