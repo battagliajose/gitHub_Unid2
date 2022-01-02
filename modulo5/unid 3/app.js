de77e00000fa72700836402965dd36fa8925b251
José Battaglia <battagliajose@gmail.com>
2022-01-02 01:38:02 -0300

    Final Clase

--- modulo5/unid 3/app.js
+++ modulo5/unid 3/app.js
@@ -25,10 +25,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-//select
-pool.query('select * from empleados').then(function (resultados){
-  console.log(resultados)
-});
+// //select
+// pool.query('select * from empleados').then(function (resultados){
+//   console.log(resultados)
+// });
+
+// //insert
+// var obj={
+//   nombre: 'Juan',
+//   apellido: 'Lopez',
+//   trabajo: 'Docente',
+//   edad: 38,
+//   salario: 1500,
+//   mail: 'juanlopez@gmail.com'
+// }
+
+// pool.query('insert into empleados set ?', [obj]).then(function (resultados){
+//   console.log(resultados)
+// });
+
+// //update
+// var id = 1;
+// var obj = {
+//     nombre: 'Pablo',
+//     apellido: 'Gomez'
+// }
+
+// pool.query('update empleados set ? where id_emp=?', [obj, id]).then(function(resultados){
+//   console.log(resultados)
+// });
+
+//delete
+var id = 8;
+pool.query('delete from empleados where id_emp=?', [id]).then(function(resultados) {
+  console.log(resultados);
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
