4ebbf943df0d73dfb34073f6ea29bf9d1fd98176
José Battaglia <battagliajose@gmail.com>
2022-01-03 22:51:59 -0300

    Mod5_U3_Tarea

--- modulo5/u3-Tarea/routes/novedades.js
+++ modulo5/u3-Tarea/routes/novedades.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config();
+var pool = require('../models/bd');
+
+// pool.query('select * from novedades').then(function (resultados){
+//   console.log(resultados)
+// });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('novedades', { title: 'Las Moras' });
+    res.render('novedades', { title: 'Las Moras' });
 });
+router.post('/', function(req, res, next) {
+  pool.query('select * from novedades', req.body.idnovedades, function(error,filas){
+            if (error) {            
+                console.log('error en la consulta');
+                return;
+            }
+            if (filas.length>0) {
+                res.render('novedades', {articulos:filas});
+            } else {
+                res.render('novedades', {mensaje:'No existe el codigo de articulo ingresado'});
+            }    
+        });
+});
+
 
 module.exports = router;
