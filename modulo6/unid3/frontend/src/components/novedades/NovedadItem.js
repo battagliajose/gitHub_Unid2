4f0d9a9aea3814591f03011d646dca34243a8eb5
José Battaglia <battagliajose@gmail.com>
2022-02-09 22:23:25 -0300

    Ajustes estilo Novedades

--- modulo6/unid3/frontend/src/components/novedades/NovedadItem.js
+++ modulo6/unid3/frontend/src/components/novedades/NovedadItem.js
@@ -4,12 +4,16 @@ const NovedadItem = (props) => {
     const { title, subtitle, imagen, body } = props;
 
     return (
-        <div className="novedades">
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-            <img src={imagen} />
-            <div dangerouslySetInnerHTML={{ __html: body}} />
-            <hr />
+        <div className="novedadesContainer holder mainDiv">
+            
+            <div className="novedad">
+                <img className="left" src={imagen}/>
+                <h2>{title}</h2>
+                <h3>{subtitle}</h3>
+                
+                <div dangerouslySetInnerHTML={{ __html: body}} />
+                <hr />
+            </div>
         </div>
     );
 }
