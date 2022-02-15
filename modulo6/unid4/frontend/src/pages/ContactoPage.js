30fdbb63ec8218c66192eac3ae00c69d918acf7e
battagliajose <battagliajose@gmail.com>
2022-02-15 09:25:45 -0300

    M6-U4 - Config. Formulario de Contacto en FrontEnd

--- modulo6/unid4/frontend/src/pages/ContactoPage.js
+++ modulo6/unid4/frontend/src/pages/ContactoPage.js
@@ -1,27 +1,66 @@
+
+import { useState } from 'react';
+import axios from 'axios';
+
 import '../styles/components/pages/ContactoPage.css'
+
 const Contacto = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const {name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:3000/api/contacto)', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if(response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
     return (
         <div className="mainDiv">
             <h2 className="contactoTitulo">Dejenos su mensaje o solicitud de reserva!</h2>
             <br/>
             <div className="formContainer">
 
-                <form onsubmit="return false">
+                <form action="/contacto" method="post" onDubmit={handleSubmit}>
                     <label for="">Nombre:</label>
-                    <input type="text" placeholder="Ingrese su nombre...." id="nombre"/>
+                    <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} placeholder="Ingrese su nombre...." id="nombre"/>
                     <br/><br/>
                     <label for="">Mail:</label>
-                    <input type="email" placeholder="Ingrese su mail..." id="mail"/>
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Ingrese su mail..." id="mail"/>
                     <br/><br/>
                     <label for="">Telefono:</label>
-                    <input type="text" placeholder="Ingrese su telefono..." id="telefono"/>
+                    <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} placeholder="Ingrese su telefono..." id="telefono"/>
                     <br/><br/>
                     <label for="">Mensaje:</label>
                     <br/><br/>
-                    <textarea cols="30" rows="10" placeholder="Ingrese su mensaje..."></textarea>
+                    <textarea cols="30" rows="10" name="mensaje" value={formData.mensaje} onChange={handleChange} placeholder="Ingrese su mensaje..."></textarea>
                     <br/><br/>
                     <button onclick="enviarContacto();">Enviar</button>
                 </form>
+                {sending ? <p>Enviando...</p> : null}
+                {msg ? <p>{msg}</p> : null}
             </div>
         </div>
     );
